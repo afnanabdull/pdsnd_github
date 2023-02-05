956ecc748adbf433cc68c2aa33584127d0df2c18
Afnan Alabdulatif <afnan.a.alabdullatif@gmail.com>
2023-02-05 10:44:04 +0300

    style: Add new print statement This print statement is added to make the scope of the project clear to the user

--- bikeshare.py
+++ bikeshare.py
@@ -17,6 +17,7 @@ def get_filters():
     """
 
     print('Hello! Let\'s explore some US bikeshare data!')
+    print('This will include only three major US cities: new_york_city, Chicago, and washington')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
     while True:
